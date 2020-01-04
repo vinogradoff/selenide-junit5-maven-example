bacc96aa592c39b417cf5d87b048fdd35ff3cf45
Alexei Vinogradov <coding@vinogradoff.de>
2020-01-04 23:29:23 +0100

    Update GoogleTest.java

--- src/test/java/de/vinogradoff/selenide/example/GoogleTest.java
+++ src/test/java/de/vinogradoff/selenide/example/GoogleTest.java
@@ -5,8 +5,6 @@ package de.vinogradoff.selenide.example;
 import de.vinogradoff.selenide.example.widgetobjects.*;
 import org.junit.jupiter.api.*;
 
-import javax.xml.bind.SchemaOutputResolver;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.CollectionCondition.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -28,10 +26,5 @@ public class GoogleTest
         SearchResults results = new SearchResults();
         results.found.shouldHave(sizeGreaterThan(1));
         results.getResult(0).shouldHave(text("Selenide: concise UI tests in Java"));
-
-        System.out.println("Parameters used for test:"+
-                " googleid.username="+System.getProperty("googleid.username")+
-                " googleid.password="+System.getProperty("googleid.password")
-        );
     }
 }
